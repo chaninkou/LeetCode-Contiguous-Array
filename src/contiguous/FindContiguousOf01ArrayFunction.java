b48348d5381d70f54d62a618ec28e3b3b6cca73c
chaninkou <michaelkoulove@yahoo.com>
2020-02-28 23:49:06 -0800

    Great question on using hashmap to store index number of sum.

--- src/contiguous/FindContiguousOf01ArrayFunction.java
+++ src/contiguous/FindContiguousOf01ArrayFunction.java
@@ -17,6 +17,7 @@ public class FindContiguousOf01ArrayFunction {
 		int sum = 0;
 		int max = 0;
 
+		// Whenever sum get add up to 0, we know we should add
 		map.put(0, -1);
 
 		for (int i = 0; i < nums.length; i++) {
@@ -25,7 +26,7 @@ public class FindContiguousOf01ArrayFunction {
 			if (map.containsKey(sum)) {
 				max = Math.max(max, i - map.get(sum));
 			} else {
-				map.put(sum, i);
+				map.put(sum, i); // Key wil increase if the pattern of the array is 1,1,1,1,1,1,1,1,1
 			}
 
 		}
