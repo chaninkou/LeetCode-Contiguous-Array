f05a400fcbc66b224eb24c48ec5759de05d57cdc
chaninkou <michaelkoulove@yahoo.com>
2020-12-28 20:47:28 -0800

    Redid problem with hashmap using total sum or counts.

--- src/leetcode525/FindContiguousOf01ArrayFunction.java
+++ src/leetcode525/FindContiguousOf01ArrayFunction.java
@@ -33,4 +33,28 @@ public class FindContiguousOf01ArrayFunction {
 
 		return max;
 	}
+	
+    public int findMaxLength2(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,-1);
+        
+        int zero=0;
+        int one=0;
+        int len=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                zero++;
+            }else{
+                one++;
+            }
+            
+            if(map.containsKey(zero-one)){
+                len=Math.max(len,i-map.get(zero-one));
+            }else{
+                map.put(zero-one,i);
+            }
+        }
+        
+        return len;
+    }
 }
